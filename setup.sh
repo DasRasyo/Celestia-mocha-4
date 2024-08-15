79b3b8f1fd230dcf887f50c7d1f9653fefe3bd5c
Conqueror <94050636+DasRasyo@users.noreply.github.com>
2024-08-15 23:33:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.14.1
+git checkout tags/v0.15.0
 make build
 make install
 make cel-key
@@ -190,7 +190,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.14.1
+git checkout tags/v0.15.0
 make build
 make install
 make cel-key
@@ -320,7 +320,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.14.1
+git checkout tags/v0.15.0
 make build
 make install
 make cel-key
